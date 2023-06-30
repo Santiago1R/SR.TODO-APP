57f40413401dcba9820c202fd9b98cde1e8d71a0
Santiago Rios <riosnelson2017@gmail.com>
2023-06-30 12:55:53 -0500

    implementación Jquery

--- js/functionalities/eliminar_tarea.js
+++ js/functionalities/eliminar_tarea.js
@@ -2,13 +2,13 @@ import { insertarTarea } from "../tareas/funcionalidad_tareas_/validationHomewor
 import { obtenerTareas } from "../tareas/creación_tareas_/contenedor_tareas.js";
 import { tareasRestantes } from "../filtros/tareas_restantes.js";
 import local from "../../module/localStorage.js";
+import { validacionEstado } from "../validacion_estado.js";
 export const eliminarTarea = function (index) {
   const tarea = obtenerTareas();
   tarea.splice(index, 1);
-  const divContent = document.getElementById("contenedor-tareas");
-  divContent.innerHTML = "";
+  $('#contenedor-tareas').empty()
   local.save("arrayTareas", tarea);
   tarea.forEach(insertarTarea);
-  const itemsLeft = document.getElementById("elements-left");
-  itemsLeft.textContent = tareasRestantes() + " items left";
+  $('#elements-left').text(tareasRestantes() + " items left")
+  validacionEstado();
 };
